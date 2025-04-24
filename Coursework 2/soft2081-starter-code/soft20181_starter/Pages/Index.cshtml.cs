85e8900eb9785f6e2b8b05877b8d5c128cec1b86
Shaheer <N1347685@ntu.ac.uk>
2025-04-24 23:17:38 +0100

    Populated Events in Index

--- Coursework 2/soft2081-starter-code/soft20181_starter/Pages/Index.cshtml.cs
+++ Coursework 2/soft2081-starter-code/soft20181_starter/Pages/Index.cshtml.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using soft20181_starter.Models;
 
 namespace soft20181_starter.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private EventAppDbContext db;
+        public IndexModel(ILogger<IndexModel> logger, EventAppDbContext db)
         {
+            this.db = db;
             _logger = logger;
         }
-
+        [BindProperty]
+        public List<Event> Events { get; set; } = new List<Event>();
         public void OnGet()
         {
-
+            Events = db.Events.Take(3).ToList();
         }
     }
 }
